7a2320506111db908bdd9a36b87376f4df94effb
CJ Moynihan <cj@whitewizardgames.com>
2023-03-04 14:54:58 -0800

    Attack progress

--- Assets/Scripts/AimController.cs
+++ Assets/Scripts/AimController.cs
@@ -7,6 +7,12 @@ public class AimController : MonoBehaviour
     private Camera mainCam;
     private Vector3 mousePos;
 
+    // Attack information
+    public GameObject attackObject;
+    public Transform attackTransform;
+    public bool canAttack;
+    public float attackCooldown;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +28,24 @@ public class AimController : MonoBehaviour
         // Calculate the amount we need to rotate
         Vector3 rotation = mousePos - transform.position;
 
+        // Apply math to rotate + 90 degrees
         float rotZ = Mathf.Atan2(-rotation.x, rotation.y) * Mathf.Rad2Deg;
-
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
 
+
+
+        // Handle attacking
+        if (Input.GetMouseButton(0) && canAttack)
+        {
+            canAttack = false;
+            Instantiate(attackObject, attackTransform.position, Quaternion.identity);
+            StartCoroutine(WaitAttackCooldown());
+        }
+    }
+
+    IEnumerator WaitAttackCooldown()
+    {
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
     }
 }
