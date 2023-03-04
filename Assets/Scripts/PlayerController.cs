5b8d418b8c1ff146c02f88db3362911d9f1ec98e
CJ Moynihan <cj@whitewizardgames.com>
2023-03-04 12:59:27 -0800

    Added basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed;
+
+    private Rigidbody2D rb;
+    // This variable will accept player movement from either keyboard or controller
+    private Vector2 playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerInput();
+    }
+
+    private void GetPlayerInput()
+    {
+        // Stick movement
+        //playerMovement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        playerMovement = new Vector2(0, 0);
+        // Keyboard movement
+        float xMovement = 0;
+        float yMovement = 0;
+        if (Input.GetKey(KeyCode.A))
+            xMovement -= 1;
+        if (Input.GetKey(KeyCode.D))
+            xMovement += 1;
+        if (Input.GetKey(KeyCode.S))
+            yMovement -= 1;
+        if (Input.GetKey(KeyCode.W))
+            yMovement += 1;
+        playerMovement += new Vector2(xMovement, yMovement);
+    }
+
+    private void FixedUpdate()
+    {
+        // Apply movement based on speed and framerate
+        Vector2 realtimeMovement = playerMovement * playerSpeed * Time.deltaTime;
+
+        rb.MovePosition(rb.position + realtimeMovement);
+    }
+}
