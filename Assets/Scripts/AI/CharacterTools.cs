8d4c4395a249c140b650778bb405e1f3e0ce6b85
CJ Moynihan <cj@whitewizardgames.com>
2023-03-05 21:09:56 -0800

    Added horn beetles\!

---
+++ Assets/Scripts/AI/CharacterTools.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTools
+{
+    //public Func<Transform> Action ShakeClosure(Transform t)
+    //{
+    //    public void Shake()
+    //    {
+    //        float shakeSpeed = 1.0f;
+    //        float shakeAmount = 1.0f;
+    //        t.position.x = Mathf.Sin(Time.time * shakeSpeed) * shakeAmount;
+    //    }
+    //    return Shake;
+    //}
+}
---
+++ Assets/Scripts/AI/HornBeetleAI.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HornBeetleAI : EnemyTypes.EnemyBehavior
+{
+    // Horn beetle moves slowly
+    // Has "big" attack
+    // Charging enemy
+    private bool chargeReady = false;
+    private bool charging = false;
+    public float rotationSpeed = 35.0f;
+    public float moveSpeed = 0.75f;
+    public float chargeSpeed = 1000f;
+    // The amount of degrees before it decides to charge
+    public float chargeMargin = 20f;
+    public float chargeBuildUp = 1f;
+
+
+    public override void Behavior()
+    {
+        Transform playerTransform = FindPlayerTransform();
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (!chargeReady)
+        {
+            // Always moving forward slowly
+            // Last attempt was just moving, instead applying forward velocity
+            //transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+            rb.AddForce(transform.up * moveSpeed * Time.deltaTime);
+
+            // Calculate rotation difference
+            float rotationAngle = (Mathf.Atan2(playerTransform.position.y - transform.position.y, playerTransform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+            Debug.Log(rotationAngle);
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, rotationAngle));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            if (System.Math.Abs((transform.rotation.eulerAngles - targetRotation.eulerAngles).z) <= chargeMargin)
+            {
+                rb.angularVelocity = 0;
+                StartCoroutine(WaitChargeAttack());
+                chargeReady = true;
+            }
+
+        } else
+        {
+            // Backup a bit
+            if (!charging)
+            {
+                rb.AddForce(transform.up * -moveSpeed * 10 * Time.deltaTime);
+            } else
+            {
+                rb.AddForce(transform.up * chargeSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    IEnumerator WaitChargeAttack()
+    {
+        yield return new WaitForSeconds(chargeBuildUp);
+        charging = true;
+    }
+
+    private Vector2 RotateMinus90(Vector2 orig)
+    {
+        return new Vector2(-orig.y, orig.x);
+    }
+
+    //private void RotateSlowly()
+    //{
+    //    throw new System.NotImplementedException();
+    //}
+
+    protected override void PlayerCollision(Collision2D collision)
+    {
+        // Deal damage, knockback self, knockback player
+        //throw new System.NotImplementedException();
+    }
+
+    protected override void AllCollision(Collision2D collision)
+    {
+        // Apply knockback and shake effect
+        chargeReady = false;
+        charging = false;
+    }
+    private void Knockback()
+    {
+        throw new System.NotImplementedException();
+    }
+    private void Shake()
+    {
+        throw new System.NotImplementedException();
+    }
+}
