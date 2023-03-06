7314a2a074410d35623c6fa466031ba886717f70
CJ Moynihan <cj@whitewizardgames.com>
2023-03-05 23:35:49 -0800

    Added damage to beetles\!

--- Assets/Scripts/AI/HornBeetleAI.cs
+++ Assets/Scripts/AI/HornBeetleAI.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HornBeetleAI : EnemyTypes.EnemyBehavior
 {
+    public void Start()
+    {
+        Debug.Log($"Health:{FindPlayer().GetComponent<PlayerController>().health}");
+    }
     // Horn beetle moves slowly
     // Has "big" attack
     // Charging enemy
@@ -30,7 +34,7 @@ public class HornBeetleAI : EnemyTypes.EnemyBehavior
 
             // Calculate rotation difference
             float rotationAngle = (Mathf.Atan2(playerTransform.position.y - transform.position.y, playerTransform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
-            Debug.Log(rotationAngle);
+            //Debug.Log(rotationAngle);
             Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, rotationAngle));
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
@@ -74,6 +78,8 @@ public class HornBeetleAI : EnemyTypes.EnemyBehavior
     {
         // Deal damage, knockback self, knockback player
         //throw new System.NotImplementedException();
+        FindPlayer().GetComponent<PlayerController>().health -= 2;
+        Debug.Log($"Health:{FindPlayer().GetComponent<PlayerController>().health}");
     }
 
     protected override void AllCollision(Collision2D collision)
