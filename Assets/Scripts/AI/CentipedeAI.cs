2476431c90aacff9d7162e69d7d4fb84cbdf180f
CJ Moynihan <cj@whitewizardgames.com>
2023-03-05 22:53:42 -0800

    Fixed centipede bug

--- Assets/Scripts/AI/CentipedeAI.cs
+++ Assets/Scripts/AI/CentipedeAI.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class CentipedeAI : EnemyTypes.EnemyBehavior
 {
-   
+    public override void Behavior()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void PlayerCollision(Collision2D collision)
+    {
+        throw new System.NotImplementedException();
+    }
 }
