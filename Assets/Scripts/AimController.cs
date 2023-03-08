3e34e24ca8ac9b297a658cb0930f5529663ca953
CJ Moynihan <cj@whitewizardgames.com>
2023-03-07 22:20:39 -0800

    Added fancy ant turning and movement

--- Assets/Scripts/AimController.cs
+++ Assets/Scripts/AimController.cs
@@ -11,13 +11,21 @@ public class AimController : MonoBehaviour
     // Attack information
     public GameObject attackObject;
     public Transform attackTransform;
-    public bool canAttack;
+    public bool canAttack = true;
+    public float attackRotation = -45;
     public float attackCooldown;
+    public int ninetyRotations = 3;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        transform.rotation = Quaternion.Euler(0, 0, (180 + 90 * ninetyRotations) % 360);
+    }
+
+    public Vector2 rotate90(Vector2 v2)
+    {
+        return new Vector2(-v2.y, v2.x);
     }
 
     // Update is called once per frame
@@ -27,16 +35,32 @@ public class AimController : MonoBehaviour
         {
             // Find mouse
             mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        } catch (MissingReferenceException)
+        }
+        catch (MissingReferenceException)
         {
             mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         }
         // Calculate the amount we need to rotate
         Vector3 rotation = mousePos - transform.position;
+        Vector2 afterRotations = new Vector2(rotation.x, rotation.y);
+        int numRotations;
+        for (numRotations = 0; numRotations < ninetyRotations; numRotations++)
+        {
+            afterRotations = rotate90(afterRotations);
+        }
 
         // Apply math to rotate + 90 degrees
-        float rotZ = Mathf.Atan2(-rotation.x, rotation.y) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        float rotZ = Mathf.Atan2(afterRotations.y, afterRotations.x) * Mathf.Rad2Deg;
+        Debug.Log(rotZ);
+
+        //if (rotZ < 180)
+        //{
+        //    transform.rotation = Quaternion.Euler(0, 0, (180 + 90 * ninetyRotations) % 360);
+        //} else
+        //{
+        //    transform.rotation = Quaternion.Euler(0, 0, 0 + (90 * ninetyRotations) % 360);
+        //}
+        transform.rotation = Quaternion.Euler(0, 0, (rotZ));
 
 
 
@@ -44,7 +68,7 @@ public class AimController : MonoBehaviour
         if (Input.GetMouseButton(0) && canAttack)
         {
             canAttack = false;
-            Instantiate(attackObject, attackTransform.position, transform.rotation * Quaternion.Euler(0, 0, -45));
+            Instantiate(attackObject, attackTransform.position, transform.rotation * Quaternion.Euler(0, 0, attackRotation));
             StartCoroutine(WaitAttackCooldown());
         }
     }
