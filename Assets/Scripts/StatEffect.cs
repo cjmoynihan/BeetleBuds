f075bf6543df764f57ebda217e1a7a411d24cb18
CJ Moynihan <cj@whitewizardgames.com>
2023-03-10 20:04:15 -0800

    Cleaned up my unused files

--- Assets/Scripts/StatEffect.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class StatEffect : MonoBehaviour
-{
-    // TODO: make Parts inherit StatEffect
-    public abstract StatsController.Stats applyStats(StatsController.Stats initialStats);
-}
