46145037efd2ce2b3accb985cebf70a29aed6f56
Kelsey <32395780+KelseyMichelle@users.noreply.github.com>
2023-03-12 22:06:35 -0700

    fixed my fmod build i think?

--- Assets/Scripts/Helpers/Script_TMPSortingLayer.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-public class Script_TMPSortingLayer : MonoBehaviour
-{
-    public int sortingLayer = 0;
-}
